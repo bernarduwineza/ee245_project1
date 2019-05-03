c381de1bc091fc4e9558ee4352fbf0e27c6bfb39
bernarduwineza <bernarduwineza@gmail.com>
2019-05-03 00:52:11 -0700

    windows

--- model/params.py
+++ model/params.py
@@ -7,31 +7,30 @@ Please feel free to use and modify this, but keep the above information. Thanks!
 
 import numpy as np
 
-mass = 0.18 # kg
-g = 9.81 # m/s/s
-I = np.array([(0.00025, 0, 2.55e-6),
-              (0, 0.000232, 0),
-              (2.55e-6, 0, 0.0003738)]);
+mass = 0.030     # kg
+g = 9.81    # m/s/s
+Ib = np.array([(1.43e-5, 0, 0),
+              (0, 1.43e-5, 0),
+              (0, 0, 2.89e-5)]);
 
-invI = np.linalg.inv(I)
-arm_length = 0.086 # meter
+invI = np.linalg.inv(Ib)
+L = 0.046  # distance from  rotor axis to center of mass (m)
 height = 0.05
 minF = 0.0
 maxF = 2.0 * mass * g
-L = arm_length
 H = height
-km = 1.5e-9
-kf = 6.11e-8
+km = 1.5e-9     # thrust constant
+kf = 6.11e-8    # moment constant
 r = km / kf
 
 #  [ F  ]         [ F1 ]
 #  | M1 |  = A *  | F2 |
 #  | M2 |         | F3 |
 #  [ M3 ]         [ F4 ]
-A = np.array([[ 1,  1,  1,  1],
-              [ 0,  L,  0, -L],
+A = np.array([[1,  1,  1,  1],
+              [0,  L,  0, -L],
               [-L,  0,  L,  0],
-              [ r, -r,  r, -r]])
+              [r, -r,  r, -r]])
 
 invA = np.linalg.inv(A)
 
