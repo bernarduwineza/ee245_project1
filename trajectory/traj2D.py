b487873875c3e46d634831765adce078ee2fa7a2
Jean Bernard Uwineza <bernarduwineza@outlook.com>
2019-05-03 11:58:43 -0700

    Trajectory

---
+++ trajectory/traj2D.py
@@ -0,0 +1,6 @@
+"""
+This file describes the mechanisms for generating a 2D trajectory for the quadrotor.
+"""
+#TODO Implement the trajectory generation --
+# For now, just a straight vertical line followed by another line,
+#   then a vertical line for landing.
---
+++ trajectory/traj3D.py
@@ -0,0 +1,5 @@
+"""
+This file describes the mechanisms for generating a 3D trajectory for the quadrotor.
+"""
+
+#TODO Implement the 3D trajectory generation --
