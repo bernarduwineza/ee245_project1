7f089d94dae2681a9eac990bf6d9b7a75f3ab8cc
bernarduwineza <bernarduwineza@gmail.com>
2019-05-29 10:27:00 -0700

    Voxel representation -> mapping

--- phase2/mapping.py
+++
@@ -1,7 +0,0 @@
-"""
-Environment and obstacle mapping using binary occupancy grids.
-Given a certain representation of obstacles, map them into a robot configuration space.
-The obstacles are 3-dimensional objects, represented by their origins, and length of sides.
-    An obstacles is represented by 6 real numbers (3: origin, 3: length of x,y,z sides)
-"""
-
