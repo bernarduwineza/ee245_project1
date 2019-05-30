3c927736dd80249931bf25e82689ac83257831df
blendid3 <44328880+blendid3@users.noreply.github.com>
2019-05-30 15:56:42 -0700

    revise it and add the position and pitch on the plt

--- phase2/phase2_try1.py
+++ phase2/phase2_try1.py
@@ -3,3 +3,15 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax_3d = Axes3D(fig)
+ax_3d.set_xlim3d(0, 20)
+ax_3d.set_ylim3d(0, 20)
+ax_3d.set_zlim3d(0, 20)
+obs1=ax_3d.bar3d(2, 2, 0, 10, 10, 10)
+##obs2=ax_3d.bar3d(8, 0, 2, 4, 1, 1)
+
+plt.show()
+
+
