e0f5055bce594a0da5d804df3a58f468e6dc6fc7
bernarduwineza <bernarduwineza@gmail.com>
2019-06-03 22:31:11 -0700

    minor changes

--- phase2/try1.py
+++ phase2/try1.py
@@ -13,12 +13,15 @@ from function_planning import draw_figure
 def main():
     obstacle_list=[]
     obstacle_exp = obstacle(location=[2, 2, 0], size=[10, 10, 10])
+    obstacle_2 = obstacle(location=[8, 5, 2], size=[0, 4, 10])
     obstacle_list.append(obstacle_exp)
+    obstacle_list.append(obstacle_2)
+    size_map = [40, 40, 40]
 
-    map=init_setting(end=(20, 20, 0))
+    map = init_setting(end=(20, 20, 10))
     path = astar(map.maze, map.start, map.end)
     print(path)
-    draw_figure(obstacle_list, path)
+    draw_figure(obstacle_list, path, size_map)
 
 
 if __name__ == '__main__':
